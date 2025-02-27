89afa3e1125c1c643e07d1c8ce6ea33a55220562
Ethan Lee <flibitijibibo@gmail.com>
2025-02-26 21:12:15 -0500

    Add SDL_SHADERCROSS_PROP_SPIRV_PSSL_COMPATIBILITY property

--- include/SDL3_shadercross/SDL_shadercross.h
+++ include/SDL3_shadercross/SDL_shadercross.h
@@ -82,6 +82,8 @@ typedef struct SDL_ShaderCross_SPIRV_Info
     SDL_PropertiesID props;                    /**< A properties ID for extensions. Should be 0 if no extensions are needed. */
 } SDL_ShaderCross_SPIRV_Info;
 
+#define SDL_SHADERCROSS_PROP_SPIRV_PSSL_COMPATIBILITY "SDL.shadercross.spirv.pssl.compatibility"
+
 typedef struct SDL_ShaderCross_HLSL_Define
 {
     char *name;   /**< The define name. */
